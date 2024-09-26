4daf4bfdc2c0b626aa1b92ff1bd1ed8360587937
SonGabriblox <102994891+SonGabriblox@users.noreply.github.com>
2024-09-26 11:32:57 -0300

    Update informaçõesGlobais.js

--- informaçõesGlobais.js
+++ informaçõesGlobais.js
@@ -1,7 +1,12 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.jso'
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
 async function vizualizaInfomacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-
+console.log(dados);
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.aa('graficos-container_texto')
+    paragrafo,innerHTML = `Você sabia que o mundo tem `
 }
+
+vizualizaInfomacoesGlobais()
